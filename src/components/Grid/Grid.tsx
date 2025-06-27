ee7802d94334edb776c0a9166d5be770a4ec985f
Juris Bandenieks <juris.bandenieks@testdrivensolutions.co.uk>
2025-06-27 14:56:18 -0700

    rowClassRule

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,6 +1,6 @@
 import { colorSchemeDark, themeBalham } from "ag-grid-community";
 import { AgGridReact } from "ag-grid-react";
-import type { FC } from "react";
+import { useMemo, type FC } from "react";
 import type { UserModel } from "@/types";
 import styles from "./Grid.module.css";
 
@@ -10,9 +10,23 @@ export const GridComponent: FC<{ data: UserModel[]; columns: any }> = ({
 }) => {
   const myTheme = themeBalham.withPart(colorSchemeDark);
 
+  const rowClassRules = useMemo(() => {
+    return {
+      // apply red to Ford cars
+      [styles.ragRed]: (params: any) => {
+        return (params.data.name as string).startsWith("K");
+      },
+    };
+  }, []);
+
   return (
     <div className={styles.gridContainer}>
-      <AgGridReact theme={myTheme} rowData={data} columnDefs={columns} />
+      <AgGridReact
+        theme={myTheme}
+        rowData={data}
+        columnDefs={columns}
+        rowClassRules={rowClassRules}
+      />
     </div>
   );
 };
